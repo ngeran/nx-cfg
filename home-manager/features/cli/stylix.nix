4ebad67ffe860302d6797fce7ece3949f03e1435
ngeran <ngeran@gmail.com>
2025-01-13 19:44:23 -0500

    added support for bluetooth headphones

--- home-manager/features/cli/stylix.nix
+++ home-manager/features/cli/stylix.nix
@@ -81,6 +81,7 @@
      image = pkgs.fetchurl {
        url = "https://codeberg.org/lunik1/nixos-logo-gruvbox-wallpaper/raw/branch/master/png/gruvbox-dark-rainbow.png";
        sha256 = "036gqhbf6s5ddgvfbgn6iqbzgizssyf7820m5815b2gd748jw8zc";
+
     };
   };
 }
