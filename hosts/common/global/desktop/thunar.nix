3bce102c390963122920f04ae3dd26ea5d8a1dee
ngeran <ngeran@gmail.com>
2025-01-18 09:34:18 -0500

    Thunar

--- hosts/common/global/desktop/thunar.nix
+++ hosts/common/global/desktop/thunar.nix
@@ -7,9 +7,8 @@ programs.thunar = {
     thunar-archive-plugin
     thunar-volman
   ];
-
-  # Optional services for Thunar functionality
+};
+ # Optional services for Thunar functionality
   services.gvfs.enable = true; # Mount, trash, and other functionalities
   services.tumbler.enable = true; # Thumbnail support for images
-};
 }
