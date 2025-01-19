7403bbdc7db5e8e7f878c11e4637776252c1f839
ngeran <ngeran@gmail.com>
2025-01-19 06:13:12 -0500

    nikos

--- hosts/common/optional/bluetooth.nix
+++ hosts/common/optional/bluetooth.nix
@@ -1,7 +1,6 @@
 {
   hardware.bluetooth.enable = true;
   hardware.pulseaudio.enable = true;
-  hardware.bluetooth.enable = true;
   hardware.bluetooth.powerOnBoot = true;
   services.blueman.enable = true;
 }
