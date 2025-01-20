86e1f44400f0990b54b26634c501d7d0099f14c9
ngeran <ngeran@gmail.com>
2025-01-20 12:41:13 -0500

    wireguard configuration

--- hosts/common/optional/wireguard/default.nix
+++ hosts/common/optional/wireguard/default.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }: {
   networking.wg-quick.interfaces = let
-    server_ip = "18.19.23.66";
+    server_ip = "216.151.180.8";
   in {
      # "wg0" is the network interface name. You can name the interface arbitrarily
     wg0 = {
@@ -13,27 +13,11 @@
       listenPort = 51820;
 
       # Path to the private key file.
-      privateKeyFile = "/etc/mullvad-vpn.key";
-
-      # This allows the wireguard server to route your traffic to the internet and hence be like a VPN
-      postUp = ''
-        ${pkgs.iptables}/bin/iptables -A FORWARD -i wg0 -j ACCEPT
-        ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.0.0.1/24 -o eth0 -j MASQUERADE
-        ${pkgs.iptables}/bin/ip6tables -A FORWARD -i wg0 -j ACCEPT
-        ${pkgs.iptables}/bin/ip6tables -t nat -A POSTROUTING -s fdc9:281f:04d7:9ee9::1/64 -o eth0 -j MASQUERADE
-      '';
-
-      # Undo the above
-      preDown = ''
-        ${pkgs.iptables}/bin/iptables -D FORWARD -i wg0 -j ACCEPT
-        ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.0.0.1/24 -o eth0 -j MASQUERADE
-        ${pkgs.iptables}/bin/ip6tables -D FORWARD -i wg0 -j ACCEPT
-        ${pkgs.iptables}/bin/ip6tables -t nat -D POSTROUTING -s fdc9:281f:04d7:9ee9::1/64 -o eth0 -j MASQUERADE
-      '';
+      privateKeyFile = "./ipvanish/nyc-c26.conf";
 
       peers = [{
         # [Peer 0] section -> PublicKey
-        publicKey = "1493vtFUbIfSpQKRBki/1d0YgWIQwMV4AQAvGxjCNVM=";
+        publicKey = "TziUEMu9VjazhEA7Y6zgqszPnCgr88rcjb+8SEhdDyM=";
         # [Peer 0] section -> AllowedIPs
         allowedIPs = [ "0.0.0.0/0" ];
         # [Peer 0] section -> Endpoint:port
