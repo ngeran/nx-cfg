efb1ce9bd04dd2235f4fd5ab5a6e7a3bca58bee0
ngeran <ngeran@gmail.com>
2025-01-20 16:13:05 -0500

    wireguard

--- hosts/common/optional/wireguard/default.nix
+++ hosts/common/optional/wireguard/default.nix
@@ -6,14 +6,14 @@
     wg0 = {
     # Determines the IP/IPv6 address and subnet of the client's end of the tunnel interface
     # IP address of this machine in the *tunnel network*
-      address = [ "172.27.200.1/24" ];
+      address = [ "100.96.5.249/32" ];
     # The port that WireGuard listens to - recommended that this be changed from default
     # To match firewall allowedUDPPorts (without this wg
     # uses random port numbers).
       listenPort = 51820;
 
       # Path to the private key file.
-      privateKeyFile = "/home/nikos/github/ngeran/nx-cfg/hosts/common/optional/wireguard/wg-private";
+      privateKeyFile = "/home/nikos/github/ngeran/nx-cfg/hosts/common/optional/wireguard/ipvanish/nyc-c26.conf";
 
       peers = [{
         # [Peer 0] section -> PublicKey
