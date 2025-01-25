52e4ced25f7a71e85c36eea1f59449b106e8292c
ngeran <ngeran@gmail.com>
2025-01-25 05:50:28 -0500

    logseq

--- home-manager/desktop.nix
+++ home-manager/desktop.nix
@@ -33,7 +33,23 @@
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
-
+  
+ # This overlay is required by LogSeq
+  nixpkgs.overlays = [
+  (
+    final: prev: {
+      logseq = prev.logseq.overrideAttrs (oldAttrs: {
+        postFixup = ''
+          makeWrapper ${prev.electron_20}/bin/electron $out/bin/${oldAttrs.pname} \
+            --set "LOCAL_GIT_DIRECTORY" ${prev.git} \
+            --add-flags $out/share/${oldAttrs.pname}/resources/app \
+            --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations}}" \
+            --prefix LD_LIBRARY_PATH : "${prev.lib.makeLibraryPath [ prev.stdenv.cc.cc.lib ]}"
+        '';
+      });
+    }
+  )
+ ];
 
  #  ------   -----   ------
   # | DP-3 | | DP-1| | DP-2 |
