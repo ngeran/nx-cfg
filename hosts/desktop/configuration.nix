9b2e4297697ac13c83a12a8e6cace8b553e30a65
ngeran <ngeran@gmail.com>
2025-01-25 05:56:08 -0500

    logseq

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -103,6 +103,22 @@
     # git
   ];
 
+   # This overlay is required by LogSeq
+  nixpkgs.overlays = [
+  (
+    final: prev: {
+      logseq = prev.logseq.overrideAttrs (oldAttrs: {
+        postFixup = ''
+          makeWrapper ${prev.electron_20}/bin/electron $out/bin/${oldAttrs.pname} \
+            --set "LOCAL_GIT_DIRECTORY" ${prev.git} \
+            --add-flags $out/share/${oldAttrs.pname}/resources/app \
+            --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations}}" \
+            --prefix LD_LIBRARY_PATH : "${prev.lib.makeLibraryPath [ prev.stdenv.cc.cc.lib ]}"
+        '';
+      });
+    }
+  )
+ ];
 
   # Power Management 
   powerManagement.powertop.enable = true;
