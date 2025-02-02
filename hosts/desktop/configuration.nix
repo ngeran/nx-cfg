f27d312c59169a2dcd41f9338c0bc0c3678a87f2
ngeran <ngeran@gmail.com>
2025-02-02 12:33:03 -0500

    neovim

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -15,7 +15,7 @@
       ./hardware-configuration.nix
       ../common/global/desktop
       ../common/global/desktop/thunar.nix
-      ../common/global/neovim
+      ../common/neovim
       ../common/optional/bluetooth.nix
       ../common/optional/pipewire.nix
       ../common/optional/qemu.nix
