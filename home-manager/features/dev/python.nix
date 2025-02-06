ae484b470a009c82a68214445d79007fc5619f40
ngeran <ngeran@gmail.com>
2025-02-06 17:44:47 -0500

    python

--- home-manager/features/dev/python.nix
+++ home-manager/features/dev/python.nix
@@ -65,7 +65,7 @@
                 jedi_completion.fuzzy = true;
 
                 pylsp_mypy.enabled = true;
-               
+                junos-eznc.enable = true;
                 # We don't need those as ruff is already providing such features.
                 autopep8.enabled = false;
                 flake8.enabled = false;
