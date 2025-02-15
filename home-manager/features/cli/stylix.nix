b9289d340c54fc15299c68164a964fb4329036af
ngeran <ngeran@gmail.com>
2025-02-15 07:36:55 -0500

    nixvim from scratch

--- home-manager/features/cli/stylix.nix
+++ home-manager/features/cli/stylix.nix
@@ -31,7 +31,7 @@
     targets = {
       firefox.enable = true; 
       alacritty.enable = true;
-      neovim.enable = false;
+      neovim.enable = true;
       waybar.enable = false;
       wofi.enable = false;
       hyprland.enable = false;
