8528890318d50840419873054a213227754aaf86
ngeran <ngeran@gmail.com>
2025-02-15 07:52:09 -0500

    nixvim from scratch

--- home-manager/features/tui/nixvim/default.nix
+++ home-manager/features/tui/nixvim/default.nix
@@ -8,17 +8,7 @@
   programs.nixvim = {
     enable = true;
    
-    colorscheme = "gruvbox";
-
-    # Enable Gruvbox plugin for better experience
-    plugins.gruvbox = {
-      enable = true;
-      settings = {
-        background = "dark"; # or "light"
-        contrast = "soft";   # Options: "soft", "medium", "hard"
-        transparent = false; # Set true for transparency
-      };
-    };
+   plugins.lualine.enable = true;
 
   };
 }
