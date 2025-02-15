e6391a5b0506543e628aa54622bf6e9a605feea9
ngeran <ngeran@gmail.com>
2025-02-15 11:14:10 -0500

    nixvim

--- home-manager/features/tui/nixvim/default.nix
+++ home-manager/features/tui/nixvim/default.nix
@@ -2,15 +2,37 @@
 {
   imports = [
     inputs.nixvim.homeManagerModules.nixvim
-    ./plugins
-    ./completion.nix
     ./autocommands.nix
+    ./completion.nix
     ./keymappings.nix
+    #./options.nix
+    ./plugins
   ];
+
+   
+  home.shellAliases.v = "nvim";
 
   programs.nixvim = {
     enable = true;
-   
+    defaultEditor = true;
 
+    nixpkgs.useGlobalPackages = true;
+
+    performance = {
+      combinePlugins = {
+        enable = true;
+        standalonePlugins = [
+          "hmts.nvim"
+          "neorg"
+          "nvim-treesitter"
+        ];
+      };
+      byteCompileLua.enable = true;
+    };
+
+    viAlias = true;
+    vimAlias = true;
+
+    luaLoader.enable = true;
   };
 }
