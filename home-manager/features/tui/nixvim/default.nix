ad122c5b224875eefd73f41189c6e0f840ee13ce
ngeran <ngeran@gmail.com>
2025-02-15 07:34:09 -0500

    nixvim from scratch

--- home-manager/features/tui/nixvim/default.nix
+++ home-manager/features/tui/nixvim/default.nix
@@ -7,8 +7,8 @@
   programs.nixvim = {
     enable = true;
    
-   colorschemes.catppuccin.enable = true;
-   plugins.lualine.enable = true;
-   
+   #colorschemes.catppuccin.enable = true;
+   #plugins.lualine.enable = true;
+
   };
 }
