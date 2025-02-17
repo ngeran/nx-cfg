bf15f4cec67e636925d66f1100e156cfb3b306c6
ngeran <ngeran@gmail.com>
2025-02-17 12:44:11 -0500

    stylix

--- home-manager/features/cli/stylix.nix
+++ home-manager/features/cli/stylix.nix
@@ -18,14 +18,6 @@
   stylix = {
     enable = true; # Enables Stylix
 
-       settings = {  # <--- This is the crucial part!
-      colors = {
-        generation = {
-          method = "wal"; # wal, average, prominent
-        };
-      };
-       };
-
     polarity = "dark"; # Sets the overall theme polarity to dark
 
     base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml"; # Sets the base16 color scheme to Gruvbox Dark Hard
