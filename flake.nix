af0d92eb30fefef8d05547e311062d2f82f97006
ngeran <ngeran@gmail.com>
2025-03-05 18:10:42 -0500

    remove stylix

--- flake.nix
+++ flake.nix
@@ -47,7 +47,7 @@
          # DESKTOP ===[Main Workstation]===
          desktop = lib.nixosSystem {
                      specialArgs = {inherit inputs outputs system;};
-                     modules = [./hosts/desktop/configuration.nix inputs.stylix.nixosModules.stylix];
+                     modules = [./hosts/desktop/configuration.nix ];
 		              };
 
          # VMWARE===[VMWARE INSTALLATION]===
