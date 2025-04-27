cb9bdacf4c12f3aa900c2855de833c744acba04a
ngeran <ngeran@gmail.com>
2025-04-26 20:38:13 -0400

    updates

--- home-manager/features/cli/nixvim/plugins/lsp.nix
+++ home-manager/features/cli/nixvim/plugins/lsp.nix
@@ -1,12 +1,16 @@
 { pkgs, ... }: {
   programs.nixvim = {
     enable = true;
-    
+
     plugins = {
       lsp.enable = true;
       lsp.servers = {
         pylsp.enable = true;
-        ruff.enable = true; 
+        ruff.enable = true;
+        marksman.enable = true;         # Markdown
+        yamlls.enable = true;           # YAML
+        rust-analyzer.enable = true;    # Rust
+        html.enable = true;             # HTML (can help with Hugo content)
       };
     };
   };
